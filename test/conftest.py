8a6bfab1ab0c4d0469ae977e1f4df26e49e00ba1
Louis Chatain <louischatain123@gmail.com>
2026-02-19 20:43:18 +0100

    typo

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from httpx import AsyncClient, ASGITransport
-# from ..db.database import Base, get_async_db
 from db.database import Base, get_async_db
 from main import app
 import asyncio
